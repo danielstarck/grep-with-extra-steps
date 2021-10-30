c4ec43781605973df96482c5a9c0a8c1bc68cb7f
Daniel Starck <daniel.starck@kaprifolv.org>
2021-10-30 13:39:53 +0200

    Add initial backend

---
+++ backend/Controllers/HelloWorldController.fs
@@ -0,0 +1,12 @@
+﻿namespace GrepWithExtraSteps.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("helloworld")>]
+type WeatherForecastController(logger: ILogger<WeatherForecastController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member _.Get() = "Hello world!"
---
+++ backend/Startup.fs
@@ -0,0 +1,31 @@
+namespace GrepWithExtraSteps
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.AspNetCore.Cors.Infrastructure
+
+type Startup(configuration: IConfiguration) =
+    member _.Configuration = configuration
+
+    member _.ConfigureServices(services: IServiceCollection) =
+        services.AddCors() |> ignore
+        services.AddControllers() |> ignore
+
+    member _.Configure(app: IApplicationBuilder, env: IWebHostEnvironment) =
+        if env.IsDevelopment() then
+            app.UseDeveloperExceptionPage() |> ignore
+
+        app
+            // .UseHttpsRedirection()
+            // Also add...
+            // "applicationUrl": "https://localhost:5001;http://localhost:5000",
+            // ...to launchSettings.json
+            .UseCors(Action<_>(fun (options: CorsPolicyBuilder) -> options.AllowAnyOrigin() |> ignore))
+            .UseRouting()
+            .UseAuthorization()
+            .UseEndpoints(fun endpoints -> endpoints.MapControllers() |> ignore)
+        |> ignore
