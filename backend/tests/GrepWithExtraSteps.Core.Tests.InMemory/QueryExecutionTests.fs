90275e8bae2389a5f7135347dd15760341fc50fe
Daniel Starck <daniel.starck@kaprifolv.org>
2021-12-08 22:31:11 +0100

    Change module to namespace

--- backend/tests/GrepWithExtraSteps.Core.Tests.InMemory/QueryExecutionTests.fs
+++ backend/tests/GrepWithExtraSteps.Core.Tests.InMemory/QueryExecutionTests.fs
@@ -1,49 +1,50 @@
-module QueryExecutionTests
+namespace GrepWithExtraSteps.Core.Tests.InMemory
 
 open FSharp.Control
+open FsCheck
 open GrepWithExtraSteps.Core
 open GrepWithExtraSteps.Core.Domain
 open Xunit
 
-let everyLineIsMatch: LineIsMatch = fun _ -> true
+type QueryExecutionTests() =
 
-let noLineIsMatch: LineIsMatch = fun _ -> false
+    let everyLineIsMatch: LineIsMatch = fun _ -> true
 
-let emptyDirectory =
-    { Directories = Seq.empty
-      Files = Seq.empty }
+    let noLineIsMatch: LineIsMatch = fun _ -> false
+
+    let emptyDirectory =
+        { Directories = Seq.empty
+          Files = Seq.empty }
     
-let someDirectory =
-    { Directories = Seq.empty
-      Files =
-          seq {
-              { Path = "/file"
-                Lines =
-                    asyncSeq {
-                        yield "line 1"
-                        yield "line 2"
-                        yield "line 3"
-                    }}
-          }}
+    let someDirectory =
+        { Directories = Seq.empty
+          Files =
+              seq {
+                  { Path = "/file"
+                    Lines =
+                        asyncSeq {
+                            yield "line 1"
+                            yield "line 2"
+                            yield "line 3"
+                        }}
+              }}
 
-[<Fact>]
-let ``searchDirectory returns an empty seq when given an empty directory`` () =
-    let resultChunks =
-        QueryExecution.searchDirectory everyLineIsMatch emptyDirectory
-        |> AsyncSeq.toListSynchronously
+    [<Fact>]
+    member _.``searchDirectory returns an empty seq when given an empty directory``() =
+        let resultChunks =
+            QueryExecution.searchDirectory everyLineIsMatch emptyDirectory
+            |> AsyncSeq.toListSynchronously
 
-    do Assert.Empty(resultChunks)
-    
-[<Fact>]
-let ``searchDirectory does not return empty result chunks`` () =
-    let resultChunks =
-        QueryExecution.searchDirectory noLineIsMatch someDirectory
-        |> AsyncSeq.toListSynchronously
-        
-    do 
-        resultChunks
-        |> Seq.iter Assert.NotEmpty
-    
+        do Assert.Empty(resultChunks)
+
+    [<Fact>]
+    member _.``searchDirectory does not return empty result chunks``() =
+        let resultChunks =
+            QueryExecution.searchDirectory noLineIsMatch someDirectory
+            |> AsyncSeq.toListSynchronously
+
+        do resultChunks |> Seq.iter Assert.NotEmpty
+
 // TODO: figure out how to generate Directory
 
 // ``searchDirectory returns an empty seq when given noLineIsMatch`` (for arbitrary directory)
