51e0793150863ec9a6d9cf50046dfbeba0c0fd4c
Daniel Starck <daniel.starck@kaprifolv.org>
2021-12-14 13:45:23 +0100

    Add do

--- backend/src/GrepWithExtraSteps/SomeHub.fs
+++ backend/src/GrepWithExtraSteps/SomeHub.fs
@@ -10,14 +10,14 @@ type SomeHub(logger: ILogger<SomeHub>, queryJobService: IQueryJobService) =
     inherit Hub()
 
     member _.StartQuery(query: Query) : Task =
-        logger.LogInformation $"StartQuery: %A{query}"
+        do logger.LogInformation $"StartQuery: %A{query}"
 
         do queryJobService.StartQueryJob query
 
         Task.CompletedTask
 
     member _.CancelQuery() : Task =
-        logger.LogInformation "CancelQuery: no payload"
+        do logger.LogInformation "CancelQuery: no payload"
 
         do queryJobService.CancelQueryJob()
 
