55f5a68ec83c308dad45b9bb360b27a1e38f148d
Daniel Starck <daniel.starck@kaprifolv.org>
2021-12-15 11:39:34 +0100

    Delay creation of StreamReaders until requested

--- backend/tests/GrepWithExtraSteps.Core.Tests.InMemory/Fakes/FileSystemService.fs
+++ backend/tests/GrepWithExtraSteps.Core.Tests.InMemory/Fakes/FileSystemService.fs
@@ -1,17 +1,37 @@
 ﻿namespace GrepWithExtraSteps.Core.Tests.InMemory.Fakes
 
 open System.IO
+open System.Text
 open GrepWithExtraSteps.Core.Interfaces
 
 type internal FileSystemService
     (
         directoriesByPath: Map<string, string seq>,
         filesByPath: Map<string, string seq>,
-        readersByPath: Map<string, StreamReader>
+        fileContentsByPath: Map<string, string seq>
     ) =
+    let linesToStreamReader lines =
+        let bytes =
+            lines
+            |> String.concat "\n"
+            |> Encoding.UTF8.GetBytes
+
+        let stream = new MemoryStream(bytes)
+
+        new StreamReader(stream)
+        
     interface IFileSystemService with
-        member this.GetDirectories path = Map.find path directoriesByPath
+        member this.GetDirectories path =
+            match Map.tryFind path directoriesByPath with
+            | Some directories -> directories
+            | None -> Seq.empty
 
-        member this.GetFiles path = Map.find path filesByPath
+        member this.GetFiles path =
+            match Map.tryFind path filesByPath with
+            | Some files -> files
+            | None -> Seq.empty
 
-        member this.GetReader path = Map.find path readersByPath
+        member this.GetReader path =
+            match Map.tryFind path fileContentsByPath with
+            | Some lines -> linesToStreamReader lines
+            | None -> linesToStreamReader Seq.empty
