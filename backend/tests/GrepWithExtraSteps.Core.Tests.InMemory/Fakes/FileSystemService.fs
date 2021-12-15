31e1622bf5d512b588473ffd30adb5d4f2d5cc6e
Daniel Starck <daniel.starck@kaprifolv.org>
2021-12-15 11:40:11 +0100

    Refactor fake FileSystemService

--- backend/tests/GrepWithExtraSteps.Core.Tests.InMemory/Fakes/FileSystemService.fs
+++ backend/tests/GrepWithExtraSteps.Core.Tests.InMemory/Fakes/FileSystemService.fs
@@ -8,30 +8,23 @@ type internal FileSystemService
     (
         directoriesByPath: Map<string, string seq>,
         filesByPath: Map<string, string seq>,
-        fileContentsByPath: Map<string, string seq>
+        linesByPath: Map<string, string seq>
     ) =
-    let linesToStreamReader lines =
-        let bytes =
-            lines
-            |> String.concat "\n"
-            |> Encoding.UTF8.GetBytes
+    let getValueOrEmptySeq key =
+        Map.tryFind key >> Option.defaultValue Seq.empty
 
-        let stream = new MemoryStream(bytes)
-
-        new StreamReader(stream)
-        
     interface IFileSystemService with
         member this.GetDirectories path =
-            match Map.tryFind path directoriesByPath with
-            | Some directories -> directories
-            | None -> Seq.empty
+            getValueOrEmptySeq path directoriesByPath
 
-        member this.GetFiles path =
-            match Map.tryFind path filesByPath with
-            | Some files -> files
-            | None -> Seq.empty
+        member this.GetFiles path = getValueOrEmptySeq path filesByPath
 
         member this.GetReader path =
-            match Map.tryFind path fileContentsByPath with
-            | Some lines -> linesToStreamReader lines
-            | None -> linesToStreamReader Seq.empty
+            let bytes =
+                getValueOrEmptySeq path linesByPath
+                |> String.concat "\n"
+                |> Encoding.UTF8.GetBytes
+
+            let stream = new MemoryStream(bytes)
+
+            new StreamReader(stream)
