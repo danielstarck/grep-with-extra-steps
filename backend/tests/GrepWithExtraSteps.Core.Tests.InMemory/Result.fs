f3d20db7756132628eb53dd4c719d6ab63ec40ee
Daniel Starck <daniel.starck@kaprifolv.org>
2021-12-19 12:53:58 +0100

    Add Result.getUnsafe

---
+++ backend/tests/GrepWithExtraSteps.Core.Tests.InMemory/Result.fs
@@ -0,0 +1,7 @@
+﻿[<RequireQualifiedAccess>]
+module internal GrepWithExtraSteps.Core.Tests.InMemory.Result
+
+let rec getUnsafe =
+    function
+    | Ok ok -> ok
+    | Error error -> failwith $"%s{nameof getUnsafe} failed because the result was of type Error: %A{error}."
